46619a30459e1714f301d1ee40c64b44d6ff1004
SnewZz <64595565+SnewZz@users.noreply.github.com>
2024-02-16 10:51:50 +0100

    Start cleaning strategies (BFS)

--- previous_work/notebooks/test.py
+++ previous_work/notebooks/test.py
@@ -1,18 +1,43 @@
-import numpy as np
-from scipy.stats import rankdata
+# And a queue that only has enqueue and dequeue operations
+class Queue:
 
-# Example array
-preds = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3])
+    def __init__(self):
+        self.queue = []
 
-preds2 = -preds
+    def enqueue(self, item):
+        self.queue.append(item)
 
-# Assign ranks using rankdata
-rk_data = rankdata(preds2, method='ordinal')
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
 
-argsort = preds2.argsort()
+    def size(self):
+        return len(self.queue)
 
-# Display the results
-print("Original array:", preds)
-print("Original negative array:", -preds)
-print("Ranks:", rk_data)
-print("argsort:", argsort)
+    def clear(self):
+        del self.queue[:]
+
+    def get(self, i):
+        return self.queue[i]
+    
+    def setter(self, i, v):
+        self.queue[i] = v
+
+mask = [False] * 3
+t_score = 10000
+is_solved = 0
+q = Queue()
+q.enqueue((mask, t_score, is_solved))
+print(q.size())
+mask, t_score, is_solved = q.dequeue()
+print(mask)
+print(sum(mask))
+while sum(mask) == 4:
+    q_data = q.dequeue()
+    if q_data is None: break
+    mask, t_score, is_solved = q_data
+if is_solved == 2:
+    print("youpi")
+print((is_solved, mask))
+print(q.size())
